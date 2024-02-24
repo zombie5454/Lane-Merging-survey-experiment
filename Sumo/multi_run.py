61f3a6f7a4c7d984524c1fdd8d0c5f6059725904
zombie5454 <b08902129@ntu.edu.tw>
2024-02-24 21:16:39 +0800

    use csv package

--- Sumo/multi_run.py
+++ Sumo/multi_run.py
@@ -1,4 +1,5 @@
 from function.scenario import scenario
+import csv
 
 # 這支程式會跑多次simulation, 然後將結果統計到output.csv裡
 # 統計在不同W_plus的大小, 搭配不同演算法產生的結果
@@ -10,36 +11,34 @@ def run3(tot, s):
 	tot[2] += s.run(arg=['', '--c', 'nowaiting'])
 	return 
 
-f = open('output.csv', 'w')
-f.write('')
-f.close()
 
-f = open('output.csv', 'a')
-f.write('W=_W+,noChange,allChange,noWaiting\n')
 
-normal_speed = 14
+s = scenario()
+
+normal_speed = s.normal_speed
 W_equal=normal_speed * 1
 W_plus=normal_speed * 1
 numA = 10
 numB = 10
+s.set_numA(numA)
+s.set_numB(numB)
 
-f.write(f'numA={numA},numB={numB}\n')
 
-s = scenario()
-s.set_numA(10)
-s.set_numB(10)
 
-for i in range(11):
-	s.set_W_plus(W_plus)
-	tot = [0, 0, 0]
-	for j in range(3):
-		run3(tot, s)
-	print('.')
-	f.write(f'{W_equal}_{W_plus},')
-	tot = list(map(lambda i: str(i/10), tot))	
-	f.write(','.join(tot))
-	f.write('\n')
-	W_plus += normal_speed * 0.2
+with open('output.csv', 'w', newline='') as f:
+	writer = csv.writer(f)
+	writer.writerow([f'numA={s.numA}', f'numB={s.numB}'])
+	writer.writerow(['W=', 'W+', 'noChange', 'allChange', 'noWaiting'])
+	
+	for i in range(11):
+		s.set_W_plus(W_plus)
+		tot = [0, 0, 0]
+		for j in range(3):
+			run3(tot, s)
+		print('.')
+		tot = list(map(lambda i: str(i/10), tot))	
+		writer.writerow([f'{W_equal}', f'{W_plus}'] + tot)
+		W_plus += normal_speed * 0.2
 
 
 
